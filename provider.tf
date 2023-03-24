e84ffb71552bea69fbf624377e2f489e6d605fcf
mcbsf <mariocbsf@gmail.com>
2023-03-24 18:25:31 +0000

    remove creds code

--- provider.tf
+++ provider.tf
@@ -1,13 +1,9 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
 } // NOT GOOD PRATICE, HIDE KEYS IN ~/.aws/credentials with aws cli after remove aws-vault and format
 
 provider "aws" {
   alias = "provider2"
   region     = "us-east-2"
-  access_key = var.AWS_ACCESS_KEY_ID
-  secret_key = var.AWS_SECRET_ACCESS_KEY
 } // NOT GOOD PRATICE, HIDE KEYS IN ~/.aws/credentials with aws cli after remove aws-vault and format
 
