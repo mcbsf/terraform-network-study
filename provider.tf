2860fa3fd9b680120f554a6dc8dc8a256ac93cb4
mcbsf <mariocbsf@gmail.com>
2023-03-24 17:54:27 +0000

    aws creds in env var to access on tf cloud env

--- provider.tf
+++ provider.tf
@@ -1,13 +1,13 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = file("./sensitive_data/access_key.txt")
-  secret_key = file("./sensitive_data/secret_key.txt")
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 } // NOT GOOD PRATICE, HIDE KEYS IN ~/.aws/credentials with aws cli after remove aws-vault and format
 
 provider "aws" {
   alias = "provider2"
   region     = "us-east-2"
-  access_key = file("./sensitive_data/access_key.txt")
-  secret_key = file("./sensitive_data/secret_key.txt")
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 } // NOT GOOD PRATICE, HIDE KEYS IN ~/.aws/credentials with aws cli after remove aws-vault and format
 
